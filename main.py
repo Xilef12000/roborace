ec6957c34c17c97c2c201482444dbc9e9b1c2147
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2021-11-22 20:00:41 +0100

    added  gyro

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ ev3 = EV3Brick()
 motorLeft = Motor(Port.C)
 motorRight = Motor(Port.B)
 colorSensor = ColorSensor(Port.S2)
+gyroSensor = GyroSensor(Port.S4)
 #touchSensor = TouchSensor(Port.S1)
 #watch = StopWatch()
 
@@ -21,23 +22,25 @@ speedCorrectorBlack = 1.3
 speedCorrectorWhite = 1.5
 
 ev3.speaker.beep()
-
+gyroSensor.reset_angle(0)
 #while not touchSensor.pressed():
 #    #wait(10)
 #    pass
 
 while True:
   color = colorSensor.reflection()
-  print(color)
+  angle = gyroSensor.angle()
+  print(angle)
+  #print(color)
   #motorLeft.run(int(((speed * (color / target) ** targetCorrector) ** speedCorrector)))
   #motorRight.run(int((speed * ((target - color + target) / target) ** targetCorrector) ** speedCorrector))
-  if (color / target > 1):
-    color = color * 1.4
-    motorLeft.run((speed * (color / target) * speedCorrectorWhite))
-    motorRight.run((speed * ((target - color + target) / target) / speedCorrectorWhite))
-  elif (color / target < 1):
-    motorLeft.run((speed * (color / target) / speedCorrectorBlack))
-    motorRight.run((speed * ((target - color + target) / target) * speedCorrectorBlack))
-  else:
-    motorLeft.run((speed * (color / target)))
-    motorRight.run((speed * ((target - color + target) / target)))
+  #if (color / target > 1):
+  #  color = color * 1.4
+  #  motorLeft.run((speed * (color / target) * speedCorrectorWhite))
+  #  motorRight.run((speed * ((target - color + target) / target) / speedCorrectorWhite))
+  #elif (color / target < 1):
+  #  motorLeft.run((speed * (color / target) / speedCorrectorBlack))
+  #  motorRight.run((speed * ((target - color + target) / target) * speedCorrectorBlack))
+  #else:
+  #  motorLeft.run((speed * (color / target)))
+  #  motorRight.run((speed * ((target - color + target) / target)))
