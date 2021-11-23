435987fa9d505736443a81a779d2cb45ac6992d6
Xilef12000 <manuel.koenig@online.de>
2021-11-23 18:39:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ import time
 ev3 = EV3Brick()
 motorLeft = Motor(Port.C)
 motorRight = Motor(Port.B)
+motorShoot = Motor(Port.A)
 colorSensor = ColorSensor(Port.S2)
 gyroSensor = GyroSensor(Port.S4)
 #touchSensor = TouchSensor(Port.S1)
 #watch = StopWatch()
 
+angleTarget = 0
+angleTargetAccuracy = 5
+speedOffset = 79.436
+turnSpeed = 10
 target = 42
 target = colorSensor.reflection()
 targetCorrector = 1
@@ -34,7 +39,7 @@ angle = 0
 while True:
   color = colorSensor.reflection()
   #angle = gyroSensor.angle()
-  speed = gyroSensor.speed() - 79.436
+  speed = gyroSensor.speed() - speedOffset
   timeOld = timeNow
   timeNow = time.time()
   timeDelta = timeNow - timeOld
@@ -46,16 +51,30 @@ while True:
   #if len(angleAverage) >= 1000:
   #  print(sum(angleAverage)/len(angleAverage))
 
-  #print(color)
-  #motorLeft.run(int(((speed * (color / target) ** targetCorrector) ** speedCorrector)))
-  #motorRight.run(int((speed * ((target - color + target) / target) ** targetCorrector) ** speedCorrector))
-  #if (color / target > 1):
-  #  color = color * 1.4
-  #  motorLeft.run((speed * (color / target) * speedCorrectorWhite))
-  #  motorRight.run((speed * ((target - color + target) / target) / speedCorrectorWhite))
-  #elif (color / target < 1):
-  #  motorLeft.run((speed * (color / target) / speedCorrectorBlack))
-  #  motorRight.run((speed * ((target - color + target) / target) * speedCorrectorBlack))
-  #else:
-  #  motorLeft.run((speed * (color / target)))
-  #  motorRight.run((speed * ((target - color + target) / target)))
+  print(color)
+  if (color < 70):
+    #motorLeft.run(int(((speed * (color / target) ** targetCorrector) ** speedCorrector)))
+    #motorRight.run(int((speed * ((target - color + target) / target) ** targetCorrector) ** speedCorrector))
+    if (color / target > 1):
+      color = color * 1.4
+      motorLeft.run((speed * (color / target) * speedCorrectorWhite))
+      motorRight.run((speed * ((target - color + target) / target) / speedCorrectorWhite))
+    elif (color / target < 1):
+      motorLeft.run((speed * (color / target) / speedCorrectorBlack))
+      motorRight.run((speed * ((target - color + target) / target) * speedCorrectorBlack))
+    else:
+      motorLeft.run((speed * (color / target)))
+      motorRight.run((speed * ((target - color + target) / target)))
+  else:
+    while True:
+      if angle < angleTarget - angleTargetAccuracy:
+        motorLeft.run(turnSpeed)
+        motorRight.run(-turnSpeed)
+      elif angle > angleTarget + angleTargetAccuracy:
+        motorLeft.run(-turnSpeed)
+        motorRight.run(turnSpeed)
+      else:
+        motorLeft.run(0)
+        motorRight.run(0)
+        motorShoot.run(0)
+        break
