0006943c1a53b6cad7afb2ed82ce80f1311dd260
Xilef12000 <manuel.koenig@online.de>
2021-12-01 13:15:07 +0100

    Update gyro.py

--- gyro.py
+++ gyro.py
@@ -35,69 +35,15 @@ timeNow = time.time()
 angle = 0
 angleAverage = []
 while True:
-  while True:
-    #angle = gyroSensor.angle()
-    AngleSpeed = gyroSensor.speed() - speedOffset
-    timeOld = timeNow
-    timeNow = time.time()
-    timeDelta = timeNow - timeOld
-    angleDelta = AngleSpeed * timeDelta
-    angle += angleDelta
-    #print(angle)
-    color = colorSensor.reflection()
-    angleAverage.append(gyroSensor.speed())
-    if len(angleAverage) >= 1000:
-      print(sum(angleAverage)/len(angleAverage))
-
-  #print(color)
-  if (color < 90):
-    print("grayscale")
-    #motorLeft.run(int(((speed * (color / target) ** targetCorrector) ** speedCorrector)))
-    #motorRight.run(int((speed * ((target - color + target) / target) ** targetCorrector) ** speedCorrector))
-    if (color / target > 1):
-      color = color * 1.4
-      motorLeft.run((speed * (color / target) * speedCorrectorWhite))
-      motorRight.run((speed * ((target - color + target) / target) / speedCorrectorWhite))
-    elif (color / target < 1):
-      motorLeft.run((speed * (color / target) / speedCorrectorBlack))
-      motorRight.run((speed * ((target - color + target) / target) * speedCorrectorBlack))
-    else:
-      motorLeft.run((speed * (color / target)))
-      motorRight.run((speed * ((target - color + target) / target)))
-  else:
-    while True:
-      #angle = gyroSensor.angle()
-      AngleSpeed = gyroSensor.speed() - speedOffset
-      timeOld = timeNow
-      timeNow = time.time()
-      timeDelta = timeNow - timeOld
-      angleDelta = AngleSpeed * timeDelta
-      angle += angleDelta
-      #print(angle)
-      if angle < angleTarget - angleTargetAccuracy:
-        print("turning")
-        motorLeft.run(turnSpeed)
-        motorRight.run(-turnSpeed)
-      elif angle > angleTarget + angleTargetAccuracy:
-        print("turning")
-        motorLeft.run(-turnSpeed)
-        motorRight.run(turnSpeed)
-      else:
-        while(True):
-          rgb = colorSensor.rgb()
-          if(not(rgb[0] < 30 and rgb[1] < 40 and rgb[2] > 50)):
-            print("forward")
-            motorLeft.run(turnSpeed)
-            motorRight.run(turnSpeed)
-            #print(rgb)
-          else: 
-            print("blue")
-            motorLeft.run(0)
-            motorRight.run(0)
-            motorShoot.run_angle (1000, -720, Stop.COAST, True)
-            motorLeft.run(500)
-            motorRight.run(500)
-            time.sleep(0.2)
-            break
-        break
-    
+  #angle = gyroSensor.angle()
+  AngleSpeed = gyroSensor.speed() - speedOffset
+  timeOld = timeNow
+  timeNow = time.time()
+  timeDelta = timeNow - timeOld
+  angleDelta = AngleSpeed * timeDelta
+  angle += angleDelta
+  #print(angle)
+  color = colorSensor.reflection()
+  angleAverage.append(gyroSensor.speed())
+  if len(angleAverage) >= 1000:
+    print(sum(angleAverage)/len(angleAverage))
